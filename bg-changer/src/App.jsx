d8a77c284cc212a3f73c94530a2b8ba51f9099f2
KDM <mehtakrishna1710@gmail.com>
2025-03-13 17:26:05 +0530

    final

--- bg-changer/src/App.jsx
+++ bg-changer/src/App.jsx
@@ -1,24 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState()
+  const [color , setColor] = useState("red")
+
 
 
 
   return (
-    <>
+    
      
-      <div className='text-center mt-5'>
-        <button className='bg-red-400 px-4 py-2 rounded mr-4'>Red</button>
-        <button className='bg-green-400 px-4 py-2 rounded mr-4'>Green</button>
-        <button className='bg-blue-200 px-4 py-2 rounded mr-4'>blue</button>
-        <button className='bg-gray-800 px-4 py-2 rounded mr-4 text-white'>Black</button>
-        <button className='bg-white-200 px-4 py-2 rounded border border-black'>white</button>
+      <div className='w-full h-screen' style = {{backgroundColor : color}}>
+        <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 py-2 bg-gray-200'>
+          <button className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-3' onClick={() => setColor("red")}>Red</button>
+          <button className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-3' onClick={() => setColor("green")}>Green</button>
+          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-3' onClick={() => setColor("blue")}>Blue</button>
+          <button className='bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded mr-3' onClick={() => setColor("yellow")}>Yellow</button>
+          <button className='bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded mr-3' onClick={() => setColor("pink")}>Pink</button>
+          <button className='bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mr-3' onClick={() => setColor("gray")}>Gray</button>
+          <button className='bg-black hover:bg-black text-white font-bold py-2 px-4 rounded mr-3' onClick={() => setColor("black")}>Black</button>
+          <button className='bg-white hover:bg-white text-black font-bold py-2 px-4 rounded mr-3' onClick={() => setColor("white")}>White</button>
+          <button className='bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded mr-3' onClick={() => setColor("orange")}>Orange</button>
+          <button className='bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded' onClick={() => setColor("purple")}>Purple</button>
+        </div>
+        
+        
       </div>
-    </>
+   
   )
 }
 
